560b3fbd6f3673315b470d2eb320b7ad38675f8f
Sriram Vanapalli <184502@sasi.edu.in>
2024-08-11 00:14:09 +0530

    save

--- conn.php
+++ conn.php
@@ -1,18 +1,18 @@
 <?php
-$servername = "localhost";
-$username = "u711900092_bte"; // your database username
-$password = "Nimda@062024"; // your database password
-$dbname = "u711900092_bte"; // your database name
+$servername = "localhost:5500"; // e.g., mysql.hostinger.com
+$username = "u711900092_bte"; // Your Hostinger database username
+$password = "Nimda@062024"; // Your Hostinger database password
+$dbname = "u711900092_bte"; // Your Hostinger database name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
+    echo "Failed to connect to MySQL: " . $conn -> connect_error;
     die("Connection failed: " . $conn->connect_error);
 }
-
-// If you want to echo a success message for testing purposes, uncomment the following line
-echo "Connected successfully";
-
+else{
+    echo "Connected successfully";
+}
 ?>
